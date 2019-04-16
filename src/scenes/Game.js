886523ed61b37f22c59f52ea72655d812a5363e0
Gabe Gonzalez <gabegonzalez24@gmail.com>
2019-04-15 21:11:18 -0700

    setup workspace

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,36 +5,20 @@ export default class extends Phaser.Scene {
     super({ key: 'GameScene' })
   }
   init () {
-    this.gameOver = false
   }
+
   preload () {}
 
   create () {
     this.add.image(400, 300, 'sky')
 
-    this.addPlatforms()
     this.addPlayer()
-    this.addCollectibles()
-    this.addBombs()
     this.addAnimations()
-    this.addColliders()
-    this.addScoreboard()
     this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   update () {
     this.getInput()
-  }
-
-  addPlatforms () {
-    this.platforms = this.physics.add.staticGroup()
-    this.platforms.create(400, 568, 'platform')
-      .setScale(2)
-      .refreshBody()
-
-    this.platforms.create(600, 400, 'platform')
-    this.platforms.create(50, 250, 'platform')
-    this.platforms.create(750, 220, 'platform')
   }
 
   addPlayer () {
@@ -44,35 +28,6 @@ export default class extends Phaser.Scene {
       .setBounce(0.2)
       .setCollideWorldBounds(true)
       .setGravityY(300)
-  }
-
-  addCollectibles () {
-    this.collectibles = this.physics.add.group({
-      key: 'star',
-      repeat: 11,
-      setXY: {
-        x: 12,
-        y: 0,
-        stepX: 70
-      }
-    })
-
-    this.collectibles.children.iterate((child) => {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
-    })
-  }
-
-  addColliders () {
-    this.physics.add.collider(this.player, this.platforms)
-    this.physics.add.collider(this.player, this.bombs, this.gameOver, null, this)
-    this.physics.add.collider(this.bombs, this.platforms)
-    this.physics.add.collider(this.collectibles, this.platforms)
-
-    this.physics.add.overlap(this.player, this.collectibles, this.collectStar, null, this)
-  }
-
-  addBombs () {
-    this.bombs = this.physics.add.group()
   }
 
   addAnimations () {
@@ -121,28 +76,5 @@ export default class extends Phaser.Scene {
     if (this.cursors.up.isDown && this.player.body.touching.down) {
       this.player.setVelocityY(-530)
     }
-  }
-
-  collectStar (player, star) {
-    star.disableBody(true, true)
-    this.updateScore(10)
-  }
-
-  addScoreboard () {
-    this.score = 0
-    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' })
-  }
-
-  updateScore (value) {
-    this.score += value
-    this.scoreText.setText(`Score: ${this.score}`)
-  }
-
-  gameOver () {
-    this.physics.pause()
-    this.player.setTint(0xff0000)
-    this.player.anims.play('turn')
-
-    this.gameOver = true
   }
 }
--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -25,10 +25,7 @@ export default class extends Phaser.Scene {
       }
     )
 
-    this.load.image('bomb', 'assets/images/bomb.png')
-    this.load.image('platform', 'assets/images/platform.png')
     this.load.image('sky', 'assets/images/sky.png')
-    this.load.image('star', 'assets/images/star.png')
   }
 
   create () {
